ef68841c74afc9b7c497be3225962abdf935041e
alexissoko <59481639+alexissoko@users.noreply.github.com>
2022-07-14 12:40:15 -0300

    Update test_wiki.py

--- test_wiki.py
+++ test_wiki.py
@@ -1,4 +1,38 @@
-from tracemalloc import start
+"""
+ 
+SDET Technical Test
+ 
+Task 1 - Automated web testing 
+With a test automation framework of selenium with java, test Wikipedia's search feature by searching for "Voyager 1" and add assertions for the same.
+ 
+Task 2 - Automation 
+How to switch from frame to main window? With syntax.
+ 
+Task 3 - There are 10 pages in same window, an image is present in any page out of ten pages in same window. How will you validate this scenario with example?
+ 
+Task 4 - Can we call parent class constructor in sub class constructor i.e constructor chaining by using super keyword?
+ 
+Task 5 - DB Connection
+How would you write a code for DB connection?
+ 
+Task 6 - Automate API 
+Given the REST API: https://developers.giphy.com/docs/api/endpoint#search
+And this call to retrieve a list of GIFs: http://api.giphy.com/v1/gifs/search?q=funny+dog&api_key=dc6zaTOxFJmzC
+Your task is to extensively Automate that API endpoint with assertions using java programming language. You can use the provided api_key: it's a public key for testing out that API.
+ 
+Task 7 -Given a string say "CODE". Now create a new string with duplicates of each character in the original string and to it append the reverse of the same string (with duplicates) excluding the last character.
+ex: step: CCOODDEE > Step: EEDDOOCC > finalResult: EEDDOOC
+ 
+Task 8 -We have 10 numbers 1-10 and one button. I want to delete those numbers but should delete only even numbers – what should be the best ways to delete by clicking on the button.
+For eg: if i click on button then alternate number should be deleted.
+ 
+Task 9 -How to read Excel data through hash map?
+ 
+Task 10 -While running script, you have observed "NoSuchElementException". But you have taken the correct locator(ID, XPATh or CSS). Still, you are facing the same issue. What might be the reason?
+Deliverable
+Please, provide a GitHub repo/cloud folder with the solutions to the tasks and instructions on how to run the tests.
+ 
+"""from tracemalloc import start
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
